3dd07b848647d8e9c5880930fa6806d0a9667a47
v.kliui <viktor.kliui@gmail.com>
2019-06-05 18:42:40 +0300

    worked Message

--- bot.py
+++ bot.py
@@ -1,16 +1,36 @@
 # -*- coding: utf-8 -*-
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 
 
-def hello(bot, update, args):
-    update.message.reply_text("HIIII")
+def hello(bot, update):
+    t = update.message.text
+    if t == "qq":
+        update.send("choose 1 or 2")
+        if t == "1":
+            update.send("1 choosen")
+        if t == "2":
+            update.message.reply_text("2 choosen")
+
+
+def command(bot, update, args):
+    update.message.reply_text("COOMAND")
 
 
 updater = Updater('')
 
-updater.dispatcher.add_handler(CommandHandler("question", hello, pass_args=True))
+updater.dispatcher.add_handler(CommandHandler("zxc", command, pass_args=True))
+text = MessageHandler(Filters.text, hello)
+updater.dispatcher.add_handler(text)
 
 updater.start_polling()
 updater.idle()
+
+
+
+
+
+
+
+
